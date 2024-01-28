e151522719e9d21a3abe024ffee6a586ac9b5996
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 22:31:53 +0100

    Update create_wp.sh

--- srcs/requirements/wordpress/conf/create_wp.sh
+++ srcs/requirements/wordpress/conf/create_wp.sh
@@ -6,7 +6,7 @@ rm -f /usr/local/bin/wp
 
 sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g' /etc/php8/php-fpm.d/www.conf
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
