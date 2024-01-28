6f438c08b2d445028c09c3d74c40ca7a0f0ae637
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 21:29:26 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 	echo "[DB config] Configuring MySQL..."
-	TMP=/tmp/.tmpfile
  
-	echo "USE mysql;" > ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
-	echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
-	echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOTPASS}';" >> ${TMP}
-	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
-	echo "CREATE USER '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
+	/usr/bin/mysql --user=root <<SQL_COMMANDS
+	USE mysql;
+	FLUSH PRIVILEGES;
+	DELETE FROM mysql.user WHERE User='';
+	DROP DATABASE IF EXISTS test;
+	DELETE FROM mysql.db WHERE Db='test';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOTPASS}';
+	CREATE DATABASE ${WP_DB_NAME};
+	CREATE USER '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+	GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+	FLUSH PRIVILEGES;
+	SQL_COMMANDS
 
 	/usr/bin/mysqld --user=mysql --bootstrap < ${TMP}
 	rm -f ${TMP}
