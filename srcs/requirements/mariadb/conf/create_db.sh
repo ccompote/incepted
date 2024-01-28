e66057cfe7d12c44619356fdcb49b66bbdcaf185
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 21:16:31 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -27,7 +27,6 @@ else
 	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
 	echo "CREATE USER '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
 	echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON *.* TO '${WP_DB_USR}'@'%';" >> ${TMP}
 	echo "FLUSH PRIVILEGES;" >> ${TMP}
 
 	/usr/bin/mysqld --user=mysql --bootstrap < ${TMP}
