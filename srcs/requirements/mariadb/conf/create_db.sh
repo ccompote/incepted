c6dd005e951026375fe744bae4cfc18c28a16115
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 19:41:17 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -2,8 +2,7 @@
 
 echo "[DB config] Configuring MariaDB..."
 
-# Set ownership of the MySQL data directory
-rm -r /run/mysqld/mysqld.sock && mkdir -p /run/mysqld 
+mkdir -p /run/mysqld 
 chown -R mysql:mysql /var/lib/mysql
 
 if [ -d "/var/lib/mysql/mysql" ]
