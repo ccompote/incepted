afeeda6cde5e0830b34e2c56eb24dc13a8f9e5a0
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 22:17:09 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -3,8 +3,6 @@
 
 	echo "[DB config] Configuring MySQL..."
 	TMP=/tmp/.tmpfile
-
- 	echo $WP_DB_NAME
  
 	echo "USE mysql;" > ${TMP}
 	echo "FLUSH PRIVILEGES;" >> ${TMP}
@@ -12,10 +10,10 @@
 	echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
 	echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
 	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> ${TMP}
-	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
-	echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON *.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';" >> ${TMP}
+	echo "CREATE DATABASE $WP_DB_NAME;" >> ${TMP}
+	echo "CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS}';" >> ${TMP}
+	echo "GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';" >> ${TMP}
 	echo "FLUSH PRIVILEGES;" >> ${TMP}
 
 	# Alpine does not come with service or rc-service,
