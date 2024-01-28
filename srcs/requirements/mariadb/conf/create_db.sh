4c963550f22ae96f61c2628f8cac0d5aa50b0a23
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 22:06:14 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -13,7 +13,7 @@
 	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> ${TMP}
 	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
 	echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
+	echo "GRANT ALL PRIVILEGES ON *.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
 	echo "FLUSH PRIVILEGES;" >> ${TMP}
 
 	# Alpine does not come with service or rc-service,
