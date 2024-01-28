c29181647cebbbad28fc9feb7e1b1c8c713c4c1a
ccompote <102297466+ccompote@users.noreply.github.com>
2024-01-28 18:19:31 +0100

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -3,13 +3,12 @@
 mkdir -p /run/mysqld
 chmod 0755 /run/mysqld
 
-# Check if /var/lib/mysql/mysql exists
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
     chmod 0755 /var/lib/mysql
     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql
 
-    # Set up database with users and admin
     cat > /tmp/mysql_setup.sql <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
@@ -28,9 +27,7 @@ EOF
     rm -f /tmp/mysql_setup.sql
 fi
 
-# Allow proper ports in mariadb network config
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-# Exit script with running daemon
 exec /usr/bin/mysqld --user=mysql --console
